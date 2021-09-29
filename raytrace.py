e568d2f94281e8faa1adaa586d9350e56fa01768
ajsusa <asusa@stanford.edu>
2021-09-29 16:26:35 -0700

    implementation of concentric window design and analysis

--- raytrace.py
+++ raytrace.py
@@ -4,3 +4,284 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utilities import print_diag, IntersectionExceedsBounds
+
+
+class Ray(object):
+    def __init__(self, xy0, wavelength, n0, c='r', dxy_0=(1, 0), t_tol=1e-3):
+        self._Pr = (np.array(xy0).reshape(-1),)
+        self._Vr = (np.array(dxy_0).reshape(-1),)
+        self._n = (n0,)
+        self._wavelength = wavelength
+        self._t_tol = t_tol
+        self._success = True
+        self.color = c
+
+    @property
+    def wavelength(self):
+        return self._wavelength
+
+    @property
+    def Pr(self):
+        return self._Pr[-1]
+
+    @property
+    def Vr(self):
+        return self._Vr[-1]
+
+    @property
+    def n(self):
+        return self._n[-1]
+
+    @property
+    def xs(self):
+        return np.array(self._Pr).T[0]
+
+    @property
+    def ys(self):
+        return np.array(self._Pr).T[1]
+
+    @property
+    def Vrs(self):
+        return self._Vr
+
+    @property
+    def success(self):
+        return self._success
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def refract_circle(self, R, Q, n_new, Y_max=None, diagnostics=False, inplace=True):
+        print_diag('    Trying to calculate the refraction of the ray...', diagnostics)
+        Pr, Vr, valid = _spherical_refraction(self.Pr, self.Vr, Q, R, self.n, n_new,
+                                              Y_max=Y_max, diag=diagnostics)
+        if inplace:
+            self._Pr += (Pr,)
+            self._Vr += (Vr,)
+            self._n += (n_new,)
+            self._success *= valid
+
+        return Pr, Vr, valid
+
+    def refract_plane(self, x, n_new, Y_max=None, inplace=True):
+        # Pr = self.extend_to_x(x, inplace=inplace)
+        # valid = np.abs(Pr[1]) <= Y_max
+        Pr, Vr, valid = _plano_refraction(self.Pr, self.Vr, x, self.n, n_new, Y_max=Y_max)
+        if inplace:
+            self._Pr += (Pr,)
+            self._Vr += (Vr,)
+            self._n += (n_new,)
+            self._success *= valid
+
+        return Pr, Vr, valid
+
+    def extend_to_x(self, x, inplace=True):
+        Pr = self.Pr
+        dx = x - Pr[0]
+        if dx > 0:
+            return self.extend_dx(dx, inplace)
+
+    def extend_dx(self, dx, inplace=True):
+        # Pr = self.Pr
+        Pr = self.Pr + dx * (self.Vr / self.Vr[0])
+        if inplace:
+            self._Vr += (self.Vr,)
+            self._Pr += (Pr,)
+        return Pr
+
+    def draw(self, c=None, lw=1, ls='-', marker='', arrows=True, arrow_size=12):
+        if c is None:
+            c = self.color
+        if ls is None:
+            ls = {True: '-', False: ':'}[self._success]
+        plt.plot(self.xs, self.ys, c=c, lw=lw, ls=ls, marker=marker)
+        if arrows:
+            arrow = [(-1, 0.5), (-0.5, 0), (-1, -0.5), (1, 0)]
+            for i in [0, -1]:
+                plt.plot(self.xs[i], self.ys[i], c=c, lw=lw, ls='', marker=arrow, mfc=c, ms=arrow_size)
+
+
+class RaySet(object):
+    def __init__(self):
+        self._success = ()
+        self._failed = ()
+
+    @property
+    def success(self):
+        return self._success
+
+    @property
+    def failed(self):
+        return self._failed
+
+    def add_success(self, ray: Ray):
+        self._success += ray,
+
+    def add_failed(self, ray: Ray):
+        self._failed += ray,
+
+    def add_ray(self, ray: Ray):
+        if ray.success:
+            self._success += ray,
+        else:
+            self._failed += ray,
+
+    def rays_of_set(self, sets='all', wavelengths=None):
+        rays = ()
+        if sets in ['all', 'success']:
+            rays += self._success
+        if sets in ['all', 'failed']:
+            rays += self._failed
+        if wavelengths is None:
+            return rays
+        elif not isinstance(wavelengths, typing.Iterable):
+            wavelengths = [wavelengths]
+        return [r for r in rays if r.wavelength in wavelengths]
+
+    def ys_at_index(self, i, sets='all'):
+        return [ray.ys[i] for ray in self.rays_of_set(sets=sets)]
+
+    def ys_in(self, sets='all'):
+        return self.ys_at_index(0, sets=sets)
+
+    def ys_out(self, sets='all'):
+        return self.ys_at_index(-1, sets=sets)
+
+    def angles_at_index(self, i, sets='all', unit='rad', wavelengths=None):
+        Vrs = [ray.Vrs[i] for ray in self.rays_of_set(sets=sets, wavelengths=wavelengths)]
+        unit_factor = {'rad': 1, 'deg': 180 / np.pi, 'arcmin': 180 * 60 / np.pi, 'arcsec': 180 * 60**2 / np.pi}[unit]
+        return [(np.arctan(Vr[1] / Vr[0]) * unit_factor) for Vr in Vrs]
+
+    def angles_in(self, sets='all', unit='rad', wavelengths=None):
+        return self.angles_at_index(0, sets=sets, unit=unit, wavelengths=wavelengths)
+
+    def angles_out(self, sets='all', unit='rad', wavelengths=None):
+        return self.angles_at_index(-1, sets=sets, unit=unit, wavelengths=wavelengths)
+
+    def aberrations(self, sets='all', unit='rad', wavelengths=None):
+        ang_in = np.array(self.angles_in(sets=sets, unit=unit, wavelengths=wavelengths))
+        ang_out = np.array(self.angles_out(sets=sets, unit=unit, wavelengths=wavelengths))
+        return ang_in - ang_out
+
+    def rms_aberration(self, sets='all', unit='rad', wavelengths=None):
+        return np.sqrt(np.mean((self.aberrations(sets=sets, unit=unit, wavelengths=wavelengths)) ** 2))
+
+    def max_aberration(self, sets='all', unit='rad', wavelengths=None):
+        return np.max(np.abs(self.aberrations(sets=sets, unit=unit, wavelengths=wavelengths)))
+
+
+def _spherical_refraction(Pr, Vr, Q, R, n1, n2, Y_max=None, diag=False):
+
+    try:
+        Pr_int, in_bounds = _spherical_intersection(Pr, Vr, Q, R, Y_max=Y_max), True
+    except IntersectionExceedsBounds as err:
+        Pr_int, in_bounds = err.Pr, False
+
+    print_diag(f'{Pr}, {Pr_int}', diag)
+
+    dc = Pr_int - Q  # x,y displacement from center to intersection point
+    print_diag(dc, diag)
+
+    a_surf = math.atan(dc[1]/dc[0])
+    a_ray_i = math.atan(Vr[1] / Vr[0])
+
+    da_ray_i = a_ray_i - a_surf
+    da_ray_o = math.asin(n1 * math.sin(da_ray_i) / n2)
+
+    a_ray_o = a_surf + da_ray_o
+
+    V = np.array([1, math.tan(a_ray_o)])
+    if V[0] < 0:
+        V *= (-1, -1)
+
+    return Pr_int, V, in_bounds
+
+
+def _spherical_intersection(Pr, Vr, Q, R, Y_max=None, tol=1e-3):
+    """
+    Find intersection of line and circle
+    adapted from: https://codereview.stackexchange.com/questions/86421/line-segment-to-circle-collision-algorithm
+    """
+    # last point on line and direction
+
+    # do some linear algebra
+    a = Vr.dot(Vr)
+    b = 2 * Vr.dot(Pr - Q)
+    c = Pr.dot(Pr) + Q.dot(Q) - 2 * Pr.dot(Q) - R**2
+
+    # determine if a solution exists
+    disc = b**2 - 4 * a * c
+    if disc < 0:
+        raise Exception("Line does not intersect the circle")
+
+    # try:  # TODO: delete old troubleshooting code
+    #     if disc < 0:
+    #         raise Exception("Line does not intersect the circle")
+    # except:
+    #     raise ValueError(f"Cannot evaluate {disc} < 0:"
+    #                      f"\n\ta = {a}, b = {b}, c = {c}"
+    #                      f"\n\tVr = {Vr}, Pr = {Pr}, Q = {Q}, R = {R}")
+
+    # solve the system of equations
+    sqrt_disc = math.sqrt(disc)
+    t1 = (-b + sqrt_disc) / (2 * a)
+    t2 = (-b - sqrt_disc) / (2 * a)
+
+    # determine which intersection is next
+    if t1 > tol and t2 > tol:
+        t = min([t1, t2])
+    elif t1 > tol:
+        t = t1
+    elif t2 > tol:
+        t = t2
+    else:
+        raise Exception(f'Error detecting next intersection:\n'
+                        f'\tDistances = ({t1}, {t2})\n'
+                        f'\tTolerance = {tol}')
+
+    P_int = Pr + t * Vr
+
+    # check if the intercept occurs outside of the valid extent of the window
+    if Y_max is not None:
+        print_diag((P_int, Y_max), False)
+        if abs(P_int[1]) > Y_max:
+            # Pr += P_int
+            raise IntersectionExceedsBounds(f'Intercept Y value {P_int[1]} greater than max value {Y_max}', Pr=P_int)
+
+    return P_int
+
+
+def _plano_refraction(Pr, Vr, x_lens, n1, n2, Y_max=None):
+    """
+    calulate the intersection point and refracted direction of a ray with a vertical plane
+    :param Pr:
+    :param Vr:
+    :param x_lens:
+    :param n1:
+    :param n2:
+    :return:
+    """
+    # calculate the plano surface intersection
+    dx = x_lens - Pr[0]
+    dy = dx / Vr[0] * Vr[1]
+    Pr_int = Pr + (dx, dy)
+    # print(f'Pr =\t{Pr}\nPr_int =\t{Pr_int}\ndx =\t{dx}\ndy =\t{dy}\nx_lens =\t{x_lens}')
+
+    # calculate the transmitted ray angle
+    ang_i = math.atan(Vr[1] / Vr[0])
+    ang_o = math.asin(n1 * math.sin(ang_i) / n2)
+
+    # calculate the transmitted ray direction vector
+    V = np.array([1, math.tan(ang_o)])
+    if Vr[0] < 0:
+        V *= (-1, -1)
+    Vr = V
+
+    if Y_max is not None:
+        valid = abs(Pr_int[1]) <= Y_max
+    else:
+        valid = True
+
+    # print(f'Plano refraction result:\n\tPr =\t{Pr_int}\n\tVr =\t{Vr}\n\tvalid =\t{valid}')
+    return Pr_int, Vr, valid
--- windows.py
+++ windows.py
@@ -4,3 +4,337 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
+
+from utilities import print_diag, refractive_index
+import raytrace
+
+
+class CurvedWall(object):
+    def __init__(self, Ri: float, h: float, t: float, h_f: float = 0., material: str = 'fused_silica',
+                 design_wavelength: typing.Union[typing.SupportsFloat, typing.Iterable] = 532,
+                 n_o: typing.SupportsFloat = 1, n_i: typing.SupportsFloat = 1):
+        self._Ri = float(Ri)
+        self._h = float(h)
+        self._t_1 = float(t)
+        self._h_f = float(h_f)
+        self._material_1 = material
+        if isinstance(design_wavelength, typing.Iterable):
+            self._design_wavelength = tuple(design_wavelength)
+        else:
+            self._design_wavelength = float(design_wavelength)
+        self._n_o = float(n_o)
+        self._n_i = float(n_i)
+        self._rays = None
+
+    @property
+    def Ri(self):
+        return self._Ri
+
+    @property
+    def Qi(self):
+        return np.array([0, 0])
+
+    @property
+    def h(self):
+        return self._h
+
+    @property
+    def t_1(self):
+        return self._t_1
+
+    @property
+    def h_f(self):
+        return self._h_f
+
+    @property
+    def material_1(self):
+        return self._material_1
+
+    @property
+    def design_wavelength(self):
+        return self._design_wavelength
+
+    @property
+    def n_1(self):
+        design_wavelengths = self.design_wavelength
+        if isinstance(design_wavelengths, typing.Iterable):
+            return [refractive_index(self.material_1, wave) for wave in design_wavelengths]
+        else:
+            return refractive_index(self.material_1, design_wavelengths)
+
+    @property
+    def n_i(self):
+        return self._n_i
+
+    @property
+    def n_o(self):
+        return self._n_o
+
+    @property
+    def x_min(self):
+        """To be overwritten in sub classes"""
+        return 0
+
+    @property
+    def x_max(self):
+        """To be overwritten in sub classes"""
+        return 0
+
+    @property
+    def rays(self):
+        return self._rays
+
+    @property
+    def Y_max_i(self):
+        return self.h / 2 - self.h_f
+
+    @property
+    def Y_max_o(self):
+        return self.h / 2
+
+    @property
+    def estimated_Y_max_in(self):
+        wave = self.design_wavelength
+        if isinstance(wave, typing.Iterable):
+            wave = list(wave)[0]
+        ray = raytrace.Ray([0, self.h / 2. - self.h_f], wave, self.n_i)
+        self.ray_trace_window(ray, 'right')
+        return ray.Pr[1]
+
+    @property
+    def default_Y_range(self):
+        y0 = self.estimated_Y_max_in
+        return -y0, y0
+
+    def inplace(self, inplace):
+        return {False: copy.deepcopy(self)}.get(inplace, self)
+
+    def get_n1(self, wavelength=None):
+        if wavelength is None:
+            return self.n_1
+        return refractive_index(self.material_1, wavelength=wavelength)
+
+    def ray_trace_functions(self, side: str = 'left'):
+        funcs = self.ray_trace_exterior, self.ray_trace_middle, self.ray_trace_interior
+        if side == 'left':
+            return funcs
+        elif side == 'right':
+            return funcs[::-1]
+
+    def ray_trace_interior(self, ray: raytrace.Ray, side: str, diagnostics=False):
+        print_diag('  Tracing the inner surface', diagnostics)
+        n1 = self.get_n1(ray.wavelength)
+        if side not in ['both', 'left', 'right']:
+            pass
+        elif side == 'left':
+            ray.refract_circle(self.Ri, self.Qi, self.n_i, Y_max=self.Y_max_i, diagnostics=diagnostics)
+        elif side == 'right':
+            ray.refract_circle(self.Ri, self.Qi, n1, Y_max=self.Y_max_i, diagnostics=diagnostics)
+        elif side == 'both':
+            ray.refract_circle(self.Ri, self.Qi, self.n_i, Y_max=self.Y_max_i, diagnostics=diagnostics)
+            ray.refract_circle(self.Ri, self.Qi, n1, Y_max=self.Y_max_i, diagnostics=diagnostics)
+
+    def ray_trace_middle(self, ray: raytrace.Ray, side: str, diagnostics=False):
+        # placeholder for doublet windows
+        pass
+
+    def ray_trace_exterior(self, ray: raytrace.Ray, side: str, diagnostics=False):
+        # placeholder to be replaced with spherical or ashperical surface
+        pass
+
+    def ray_trace_window(self, ray: raytrace.Ray, side, diagnostics=False):
+        if side not in ['left', 'right']:
+            raise ValueError('"Side" must be one of "left" or "right".')
+        else:
+            print_diag(f'Tracing the {side} window', diagnostics)
+            trace_functions = self.ray_trace_functions(side)
+            for f in trace_functions:
+                f(ray, side, diagnostics)
+
+    def ray_trace_windows(self, ray: raytrace.Ray, x_stop=None, dx_extend=None, diagnostics=True):
+        self.ray_trace_window(ray, 'left', diagnostics=diagnostics)
+        self.ray_trace_window(ray, 'right', diagnostics=diagnostics)
+        if x_stop is not None:
+            ray.extend_to_x(x_stop)
+        elif dx_extend is not None:
+            ray.extend_dx(dx_extend)
+
+    def trace_ray_set(self, wavelength=None, n_rays=21, y_range=None, x_start=None, x_stop=None, dx_extend=None,
+                      append=False, color='r', diagnostics=False, draw=True, arrow_size=10, inplace=True, marker='',
+                      linestyle=None):
+        if append and inplace:
+            ray_set = self._rays
+        else:
+            ray_set = raytrace.RaySet()
+
+        if wavelength is None:
+            wavelength = self.design_wavelength
+
+        print_diag(f'y_range =\t{y_range}', False)
+        if y_range is None:
+            # y_range = -self.h / 2 + self.h_f, self.h / 2 - self.h_f
+            y_range = self.default_Y_range
+        elif isinstance(y_range, typing.Iterable):
+            pass
+        else:
+            y_range = -np.abs(y_range), np.abs(y_range)
+        print_diag(f'y_range =\t{y_range}', False)
+
+        ys_in = np.linspace(y_range[0], y_range[1], n_rays)
+
+        if x_start is None:
+            x_start = self.x_min - 0.1 * np.abs(self.x_min)
+
+        if x_stop is None and dx_extend is None:
+            x_stop = self.x_max + 0.1 * np.abs(self.x_max)
+
+        if not isinstance(wavelength, typing.Iterable):
+            wavelength = [wavelength]
+        for wave in wavelength:
+            for y in ys_in:
+                ray = raytrace.Ray((x_start, y), wave, self.n_o, c=color)
+                self.ray_trace_windows(ray, x_stop=x_stop, dx_extend=dx_extend, diagnostics=diagnostics)
+                if draw:
+                    ray.draw(ls=linestyle, marker=marker, arrow_size=arrow_size)
+                ray_set.add_ray(ray)
+
+        if inplace:
+            self._rays = ray_set
+
+        return ray_set
+
+
+class Bispherical(CurvedWall):
+    def __init__(self, Ri: float, Ro: float, h: float,
+                 t: float, h_f: float = 0., material: str = 'fused_silica',
+                 design_wavelength: typing.Union[float, typing.Iterable] = 532,
+                 n_o: float = 1., n_i: float = 1., draw=False, trace=True, n_rays=21,
+                 ray_y_range=None, x_start=None, x_stop=None, dx_extend=None, draw_rays=False):
+        super().__init__(Ri, h, t, h_f, material, design_wavelength, n_o, n_i)
+        self._Ro = Ro
+        if draw:
+            self.draw()
+        if trace:
+            if ray_y_range is None:
+                ray_y_range = self.default_Y_range
+            self.trace_ray_set(design_wavelength, n_rays=n_rays, y_range=ray_y_range,
+                               x_stop=x_stop, dx_extend=dx_extend, x_start=x_start, draw=draw_rays)
+
+    @property
+    def Ro(self) -> float:
+        return self._Ro
+
+    @property
+    def t(self):
+        return self.t_1
+
+    @property
+    def x_min(self):
+        return self.get_Qo('left')[0] - self.Ro
+
+    @property
+    def x_max(self):
+        return self.get_Qo('right')[0] + self.Ro
+
+    def get_Qo(self, side):
+        Qo_right = self.Qi + np.array(
+            [-self.Ro + self.Ri + self.t, 0])  # center coordinates of the outer surface; defined for left window
+        if side == 'right':
+            return Qo_right
+        elif side == 'left':
+            return Qo_right * np.array([-1, 0])
+
+    def ray_trace_exterior(self, ray: raytrace.Ray, side, diagnostics=False):
+        print_diag('  Tracing the outer surface', diagnostics)
+        n1 = self.get_n1(ray.wavelength)
+        if side not in ['left', 'right']:
+            pass
+        elif side == 'left':
+            ray.refract_circle(self.Ro, self.get_Qo(side), n1, Y_max=self.Y_max_o, diagnostics=diagnostics)
+        elif side == 'right':
+            ray.refract_circle(self.Ro, self.get_Qo(side), self.n_o, Y_max=self.Y_max_o, diagnostics=diagnostics)
+
+    def get_xy_i(self, n_pts=100):
+        # points on inner surface
+        rad_ext_i = math.asin((self.h / 2 - self.h_f) / self.Ri)
+        rad_i = np.linspace(-rad_ext_i, rad_ext_i, n_pts).reshape(-1)
+        xi = np.array(self.Ri * np.cos(rad_i))
+        yi = np.array(self.Ri * np.sin(rad_i))
+        return xi, yi
+
+    def get_xy_o(self, n_pts=100):
+        # points on outer surface
+        rad_ext_o = math.asin(self.h / 2 / self.Ro)
+        rad_o = np.linspace(-rad_ext_o, rad_ext_o, n_pts).reshape(-1)
+        xo = np.array(self.Ro * np.cos(rad_o) - self.get_Qo('left')[0])
+        yo = np.array(self.Ro * np.sin(rad_o))
+        return xo, yo
+
+    @staticmethod
+    def get_xy_m(_):
+        # points on mid surface; undefined except for cemented doublets
+        return 0, None
+
+    def draw(self, which='both', n_pts=100, center_circle=False, ls='-', c='k', lw=2,
+             new_figure=True, figsize=(10, 5)):
+        # # points on inner surface
+        # rad_ext_i = math.asin((self.h / 2 - self.h_f) / self.Ri)
+        # rad_i = np.linspace(-rad_ext_i, rad_ext_i, n_pts).reshape(-1)
+        # xi = self.Ri * np.cos(rad_i)
+        # yi = self.Ri * np.sin(rad_i)
+        xi, yi = self.get_xy_i(n_pts)
+
+        # # points on outer surface
+        # rad_ext_o = math.asin(self.h / 2 / self.Ro)
+        # rad_o = np.linspace(-rad_ext_o, rad_ext_o, n_pts).reshape(-1)
+        # # xo = self.Ro * np.cos(rad_o) - self.Ro + self.Ri + self.t
+        # xo = self.Ro * np.cos(rad_o) - self.get_Qo('left')[0]
+        # yo = self.Ro * np.sin(rad_o)
+        xo, yo = self.get_xy_o(n_pts)
+
+        # points on mid surface
+        xm, ym = self.get_xy_m(n_pts)
+
+        if new_figure:
+            plt.figure(figsize=figsize)
+            plt.axis('equal')
+        if center_circle:
+            circle = plt.Circle((0, 0), self.Ri, ec=c, ls=':', lw=lw * .75, fill=False)
+            plt.gca().add_patch(circle)
+
+        if which in ['left', 'both']:
+            self._draw_window(-xi, yi, -xo, yo, -xm, ym, ls=ls, c=c, lw=lw)
+        if which in ['right', 'both']:
+            self._draw_window(xi, yi, xo, yo, xm, ym, ls=ls, c=c, lw=lw)
+
+    def check_fix_geometry(self):
+        xi, _ = self.get_xy_i(2)
+        xo, _ = self.get_xy_o(2)
+        dx = xo[0] - xi[0]
+        if dx < 0:
+            self._t_1 += np.abs(dx)
+
+    @staticmethod
+    def _draw_window(xi, yi, xo, yo, xm=None, ym=None, ls='-', c='k', lw=2):
+        plt.plot(xi, yi, c=c, ls=ls, lw=lw)
+        plt.plot(xo, yo, c=c, ls=ls, lw=lw)
+        if xm is not None and ym is not None:
+            plt.plot(xm, ym, c=c, ls=ls, lw=lw)
+        plt.plot([xo[0], xi[0], xi[0]], [yo[0], yo[0], yi[0]], c=c, ls=ls, lw=lw)
+        plt.plot([xo[-1], xi[-1], xi[-1]], [yo[-1], yo[-1], yi[-1]], c=c, ls=ls, lw=lw)
+        plt.axis('equal')
+
+
+class Concentric(Bispherical):
+    @property
+    def Ro(self):
+        return self.Ri + self.t
+
+    def __init__(self, Ri: float, h: float, t: float, h_f: float = 0., material: str = 'fused_silica',
+                 design_wavelength: typing.Union[float, typing.Iterable] = 532,
+                 n_o: float = 1, n_i: float = 1, draw=False, trace=True, n_rays=21, ray_y_range=None,
+                 x_start=None, x_stop=None, dx_extend=None, draw_rays=False):
+        Ro = Ri + t
+        super().__init__(Ri, Ro, h, t, h_f=h_f, material=material, design_wavelength=design_wavelength,
+                         n_o=n_o, n_i=n_i, draw=draw, trace=trace, n_rays=n_rays, ray_y_range=ray_y_range,
+                         x_start=x_start, x_stop=x_stop, dx_extend=dx_extend, draw_rays=draw_rays)
